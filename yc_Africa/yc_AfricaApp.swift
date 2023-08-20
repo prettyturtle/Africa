5f503341392d2b3fbb6ef1e2dcb7df4ea26b8230
prettyturtle <unifconti@gmail.com>
2023-08-21 00:14:04 +0900

    Initial Commit

---
+++ yc_Africa/yc_AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  yc_AfricaApp.swift
+//  yc_Africa
+//
+//  Created by yc on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct yc_AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
