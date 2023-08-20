b79d6a702212413690ac05d0a3a739eaa18cbcc3
prettyturtle <unifconti@gmail.com>
2023-08-21 01:52:03 +0900

    CoverImageView

---
+++ yc_Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  yc_Africa
+//
+//  Created by yc on 2023/08/21.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
---
+++ yc_Africa/View/CoverImageView.swift
@@ -0,0 +1,35 @@
+//
+//  CoverImageView.swift
+//  yc_Africa
+//
+//  Created by yc on 2023/08/21.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    let coverImages: [CoverImage] = Bundle.main.decode("covers.json")
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(coverImages) { coverImage in
+                Image(coverImage.name)
+                    .resizable()
+                    .scaledToFill()
+            }
+        } //: TAB
+        .tabViewStyle(.page)
+    }
+}
+
+// MARK: - PREVIEW
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
